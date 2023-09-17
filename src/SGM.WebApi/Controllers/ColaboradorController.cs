eb16919a0b20882323f59ae1b9030160de10f752
arielgiacomini <arieltecnologia@outlook.com>
2023-09-17 09:58:38 -0300

    Atualizações de Logs

--- src/SGM.WebApi/Controllers/ColaboradorController.cs
+++ src/SGM.WebApi/Controllers/ColaboradorController.cs
@@ -4,6 +4,7 @@ using SGM.ApplicationServices.Interfaces;
 using SGM.ApplicationServices.ViewModels;
 using System;
 using System.Security.Cryptography;
+using System.Text.Json;
 
 namespace SGM.WebApi.Controllers
 {
@@ -37,6 +38,7 @@ namespace SGM.WebApi.Controllers
             catch (Exception ex)
             {
                 _logger.Error(ex, $"[ColaboradorController.GetColaboradorForAll] - Erro ao efetuar a busca de todos os colaboradores erro:{ex.Message}");
+
                 return StatusCode(500, ex);
             }
         }
@@ -56,6 +58,7 @@ namespace SGM.WebApi.Controllers
             catch (Exception ex)
             {
                 _logger.Error(ex, $"[ColaboradorController.GetColaboradorById] - Erro ao buscar um colaborador pelo ID {colaboradorId} erro:{ex.Message}");
+
                 return StatusCode(500, ex);
             }
         }
@@ -66,11 +69,16 @@ namespace SGM.WebApi.Controllers
         {
             try
             {
+                _logger.Information($"[ColaboradorController.GetColaboradorByColaboradorLogin] - Busca de um colaborador pelo login {colaboradorLogin}");
+
                 var colaborador = _colaboradorServices.GetByColaboradorLogin(colaboradorLogin);
+
                 return Ok(colaborador);
             }
             catch (Exception ex)
             {
+                _logger.Error(ex, $"[ColaboradorController.GetColaboradorByColaboradorLogin] - Erro ao buscar um colaborador pelo login {colaboradorLogin} erro:{ex.Message}");
+
                 return StatusCode(500, ex);
             }
         }
@@ -81,11 +89,16 @@ namespace SGM.WebApi.Controllers
         {
             try
             {
+                _logger.Information($"[ColaboradorControler.Salvar] - Solicitação para salvar um colaborador: {JsonSerializer.Serialize(model)}");
+
                 _colaboradorServices.Salvar(model);
+
                 return Ok();
             }
             catch (Exception ex)
             {
+                _logger.Error(ex, $"[ColaboradorControler.salvar] - Erro ao salvar um colaborador Erro:{ex.Message} com o objeto:{JsonSerializer.Serialize(model)}");
+                
                 return StatusCode(500, ex);
             }
         }
@@ -96,12 +109,17 @@ namespace SGM.WebApi.Controllers
         {
             try
             {
+                _logger.Information($"[ColaboradorControler.Atualizar] - Solicitação para salvar um colaborador: {colaboradorId} com as informações: {JsonSerializer.Serialize(model)}");
+                
                 model.ColaboradorId = colaboradorId;
                 _colaboradorServices.Atualizar(model);
+
                 return Ok();
             }
             catch (Exception ex)
             {
+                _logger.Error(ex, $"[ColaboradorControler.Atualizar] - Erro ao Atualizar o Cliente: Erro: {ex.Message} com o objeto: {JsonSerializer.Serialize(model)}");
+                
                 return StatusCode(500, ex);
             }
         }
